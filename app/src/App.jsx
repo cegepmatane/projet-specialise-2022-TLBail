a06270178fe374a1c302bfaf5befa6cc349d562e
TLBail <62939298+TLBail@users.noreply.github.com>
2022-02-08 17:29:46 -0500

    POC - #5 création d'un nft l'orsque l'utilisateur arrive sur la page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,45 +1,31 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Home from './components/Home'
+import Install from './components/Install'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [account, setAccount] = useState();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  const connectuser = async () => {
+    const [accounto] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounto);
+  };
+
+  if (window.ethereum) {
+
+    connectuser();
+
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+
+  } else {
+    return <Install />;
+  }
+
 }
 
 export default App
---
+++ app/src/components/Install.jsx
@@ -0,0 +1,10 @@
+const Install = () => {
+    return (
+        <div>
+            <h3>Tu as besoin de metamask !</h3>
+            <a href="https://metamask.io/download.html">Install metamask</a>
+        </div>);
+
+};
+
+export default Install;
