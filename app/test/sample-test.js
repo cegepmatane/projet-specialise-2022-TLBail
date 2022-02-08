f6592479e161f45576a92f59fe4035f8c91163e5
TLBail <62939298+TLBail@users.noreply.github.com>
2022-02-08 15:59:35 -0500

    POC - #4 codage du test du contract qui passe ✔

--- app/test/sample-test.js
+++ app/test/sample-test.js
@@ -1,19 +1,27 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("GoldenGames", function () {
+  it("should mint new nft and transfer an Nft to someone", async function () {
+    const GoldenGames = await ethers.getContractFactory("GoldenGames");
+    const goldenGames = await GoldenGames.deploy();
+    await goldenGames.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const recipient = '0x2546bcd3c84621e976d8185a91a922ae77ecec30';
+    const metadataURI = 'cid/test.png';
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let balance = await goldenGames.balanceOf(recipient);
+    expect(balance).to.equal(0);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const newlyMintedToken = await goldenGames.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.05') });
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await newlyMintedToken.wait();
+
+    balance = await goldenGames.balanceOf(recipient);
+    expect(balance).to.equal(1);
+
+    expect(await goldenGames.isContentOwned(metadataURI)).to.equal(true);
+
+
   });
 });
