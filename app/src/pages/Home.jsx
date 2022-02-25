a4f5cc237992e460c4f16f2b96f3ac55d802cae3
TLBail <62939298+TLBail@users.noreply.github.com>
2022-02-25 11:55:38 -0500

    mergfe

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import { Col, Container, Row, Button } from "react-bootstrap";
+import NftCard from "../components/NftCard";
+import Nft from "../components/Nft";
+import { Link } from "react-router-dom";
+function Discover() {
+
+
+    return (
+        <Container className="mt-5 text-light">
+            <Row>
+                <Col className="col-8">
+                    <div className="d-flex flex-column h-100">
+                        <h1 className="megaTextHome p-1 flex-grow-1">
+                            Découvre, collectionne, et échange d’incroyable NFT
+                        </h1>
+                        <h2 className="align-self-end font-weight-light">
+                            goldengames.games est le meilleur endroit pour gérer ces NFT
+                        </h2>
+                        <div className="d-flex">
+                            <Link className='flex-grow-1 linkNavBar btn-primary btn' to="/discover" >
+                                <h1 className="m-2 fw-bold">
+                                    Explorer
+                                </h1>
+                            </Link>
+                            <Link className='flex-grow-1 linkNavBar btn-light btn text-primary' to="/mint" >
+                                <h1 className="m-2 fw-bold">
+                                    Créer
+                                </h1>
+                            </Link>
+                        </div>
+                    </div>
+                </Col>
+                <Col>
+                    <NftCard nft={new Nft(Math.round(Math.random() * 100))} />
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-8">
+                    <a className="ml-2 p-2 linkNavBar btn-danger btn" href="https://github.com/cegepmatane/projet-specialise-2022-TLBail">
+                        ➡
+                        En Apprendre plus sur goldengames.games
+                    </a>
+                </Col>
+            </Row>
+        </Container >
+    );
+
+}
+
+
+export default Discover;
