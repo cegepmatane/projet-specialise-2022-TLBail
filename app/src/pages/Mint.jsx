032926c09b52bdab4e43afaa062ebfca558c7c9f
TLBail <62939298+TLBail@users.noreply.github.com>
2022-02-28 21:08:33 -0500

    interface Mint ton nft ! MVP - Mint d'un nft #16

--- app/src/pages/Mint.jsx
+++ app/src/pages/Mint.jsx
@@ -1,11 +1,44 @@
+import { Container, Row, Alert, Button } from 'react-bootstrap'
+
+
 function Mint() {
 
     return (
-        <div>
-            <h1>Mint page</h1>
-        </div>
+        <Container className="text-light">
+            <Row>
+                <h1 className='text-center display-1 '>Mint ton nft ! </h1>
+            </Row>
+            <Row>
+                <p>
+                    Mint un nft en cliquant sur le bouton Mint.
+                    Les frais de création sont de 0.05 eth,
+                    une fois la transaction effectuée,
+                    un nft est ajouté à ta collection de manières aléatoires.
+                    Personne ne sait qu’elle est le prochain nft créer !
+                    Tu peux déjà voir les nft mint sur la page explorer.
+                    Actuellement les nft des séries suivantes sont obtenables :
+                </p>
+            </Row>
+            <Row className='p-3'>
+                <ul>
+                    <li>
+                        Goldengames 1
+                    </li>
+                    <li>
+                        Goldengames 2
+                    </li>
+                </ul>
+            </Row>
+            <Row>
+                <div className='d-flex justify-content-center'>
+                    <Button variant="primary" size="lg" className='fs-'>
+                        Mint
+                    </Button>
+                </div>
+            </Row>
+        </Container>
     );
 
 }
-
+            
 export default Mint;
