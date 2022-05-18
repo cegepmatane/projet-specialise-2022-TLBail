e2c5cd6483630177ddf53d1fc0cf1e46464445df
TLBail <62939298+TLBail@users.noreply.github.com>
2022-05-18 18:22:09 -0400

    switch auto network

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -10,6 +10,31 @@ function App() {
   const [account, setAccount] = useState(null);
 
   const connectuser = async () => {
+    try {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x13881' }], // chainId must be in hexadecimal numbers
+      });
+    } catch (error) {
+      if (error.code === 4902) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x13881',
+                rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/KBBUDl6m0-m8bWd3GGWU-eniZKy1elh4',
+              },
+            ],
+          });
+        } catch (addError) {
+          console.error(addError);
+        }
+      } else if (error.code === -32002) {
+
+      }
+      console.error(error);
+    }
     const [accounto] = await window.ethereum.request({ method: 'eth_requestAccounts' });
     setAccount(accounto);
     UserContext.connectUser();
